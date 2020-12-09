1191746cc1de27f23235aa443ca51d3166f4e3aa
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-12-09 18:09:18 -0300

    Picking product redux state on Cart and showing it

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,44 +1,54 @@
 import React from 'react';
 
 import { Container, Footer } from './styles';
-import airmax from "../../images/airmax.png"
-import { FiTrash2 } from "react-icons/fi"
 
+import { FiTrash2 } from "react-icons/fi"
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCart } from '../../store/actions';
 
 function Cart() {
-  
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.product);
+  const total = useSelector(state => state.total);
 
+  function handleRemove(id){
+    dispatch(removeFromCart(id))
+  }
 
   return (
     <Container>
-      <table>
-        <tr>
-          <th></th>
-          <th>Produto</th>
-          <th>QTD</th>
-          <th>SubTotal</th>
-          <th></th>
-        </tr>
-        <tr>
-          <td><img src={airmax} alt="nike"/></td>
-          <td>Air Max Nike </td>
-          <td>
-            <input disabled placeholder="3" type="text"/>
-          </td>
-          <td>1000R$</td>
-          <td><FiTrash2 /></td>
-        </tr>
-      </table>
-
-      <Footer>
-        <button>Finalizar Pedido</button>
-
-        <div className="total">
-          <small>Total</small>
-          <h1>R$ 978,90 R$</h1>
+      {products.map(product => (
+        <div key={product.data.id}>
+          <table>
+            <tr>
+              <th></th>
+              <th>Produto</th>
+              <th>QTD</th>
+              <th>SubTotal</th>
+              <th></th>
+            </tr>
+            <tr>
+              <td><img src={product.data.shoe} alt="nike"/></td>
+              <td>{product.data.title}</td>
+              <td>
+                <input disabled placeholder={product.amount} type="text"/>
+              </td>
+              <td>{(product.data.price * product.amount).toFixed(2)} R$</td>
+              <td>
+                <button onClick={() => handleRemove(product.data.id)}><FiTrash2 /></button>
+              </td>
+            </tr>
+          </table>
         </div>
-      </Footer>
+      ))}
+      <Footer>
+          <button>Finalizar Pedido</button>
 
+          <div className="total">
+            <small>Total</small>
+            <h1>{total.toFixed(2)} R$</h1>
+          </div>
+      </Footer>
     </Container>
   );
 }
