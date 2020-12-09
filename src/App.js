4e540402b345f3c63766e3fdabd9f86c8eb4f398
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-12-09 17:42:54 -0300

    Wrapping all app with redux provider to all aplication have access to redux store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
+import Routes from './routes';
 
-import { BrowserRouter, Route, Switch} from "react-router-dom"
+import store from './store'
+import { Provider } from 'react-redux';
 
-import GlobalStyles from "./styles/globalStyles"
-
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
-import Header from "./components/Header"
 
 function App() {
   return (
-  <BrowserRouter>
-    <Header />
-
-    <Switch>
-      
-      <Route path="/" exact > <Home /> </Route>
-      <Route path="/cart" exact render={Cart} />
-      
-    </Switch>
-    <GlobalStyles />
-  </BrowserRouter>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   );
 }
 
